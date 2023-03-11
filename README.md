744c2e8ce6e1beb78fd3d94108b5cee3fe498cae
linweiyuan <root@linweiyuan.com>
2023-03-11 15:47:24 +0000

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# chatgpt-api
+
+**[中文](https://linweiyuan.github.io/2023/02/25/ChatGPT-Swing.html)**
+
+Unofficial API in Java.
---
+++ src/main/java/com/linweiyuan/chatgptapi/ChatgptapiApplication.java
@@ -0,0 +1,13 @@
+package com.linweiyuan.chatgptapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatgptapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatgptapiApplication.class, args);
+    }
+
+}
