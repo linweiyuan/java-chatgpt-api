85051e2c1f0655c2d0e4d170d508f09da29ae180
linweiyuan <root@linweiyuan.com>
2023-03-21 15:05:21 +0000

    update doc

--- src/main/java/com/linweiyuan/chatgptapi/config/WebDriverConfig.java
+++ src/main/java/com/linweiyuan/chatgptapi/config/WebDriverConfig.java
@@ -27,12 +27,12 @@ public class WebDriverConfig {
         chromeOptions.addArguments("--disable-gpu");
         chromeOptions.addArguments("--headless=new");
 
-        var proxyServer = System.getenv("PROXY_SERVER");
+        var proxyServer = System.getenv("HTTP_PROXY_SERVER");
         if (proxyServer != null) {
             chromeOptions.addArguments("--proxy-server=" + proxyServer);
         }
 
-        var webDriver = new RemoteWebDriver(new URL(System.getenv("WEB_DRIVER_URL")), chromeOptions);
+        var webDriver = new RemoteWebDriver(new URL(System.getenv("CHATGPT_PROXY_SERVER")), chromeOptions);
         webDriver.get(Constant.CHATGPT_URL);
 
         webDriver.manage().timeouts().scriptTimeout(Duration.ofSeconds(Constant.SCRIPT_EXECUTION_TIMEOUT));
