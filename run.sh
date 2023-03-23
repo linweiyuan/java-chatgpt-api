2524b7e3c8cf077107c842b076b7cd1292139523
linweiyuan <root@linweiyuan.com>
2023-03-23 17:24:23 +0000

    update docker config

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 ./gradlew build
 
+java -Djarmode=layertools -jar build/libs/java-chatgpt-api-1.0.0.jar extract --destination build/libs/extracted
+
+docker-compose build
+
 docker-compose up -d
 
 docker-compose logs -f java-chatgpt-api
