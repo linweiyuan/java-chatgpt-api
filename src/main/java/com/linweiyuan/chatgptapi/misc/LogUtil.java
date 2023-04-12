04ea9ac2bd94c702881bcd21aa58029178ae8f1c
linweiyuan <root@linweiyuan.com>
2023-04-12 03:12:50 +0000

    update logging style to bold

--- src/main/java/com/linweiyuan/chatgptapi/misc/LogUtil.java
+++ src/main/java/com/linweiyuan/chatgptapi/misc/LogUtil.java
@@ -3,18 +3,19 @@ package com.linweiyuan.chatgptapi.misc;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ansi.AnsiColor;
 import org.springframework.boot.ansi.AnsiOutput;
+import org.springframework.boot.ansi.AnsiStyle;
 
 @Slf4j
 public class LogUtil {
     public static void info(String msg) {
-        log.info(AnsiOutput.toString(AnsiColor.GREEN, msg, AnsiColor.DEFAULT));
+        log.info(AnsiOutput.toString(AnsiStyle.BOLD, AnsiColor.GREEN, msg, AnsiColor.DEFAULT));
     }
 
     public static void warn(String msg) {
-        log.warn(AnsiOutput.toString(AnsiColor.YELLOW, msg, AnsiColor.DEFAULT));
+        log.warn(AnsiOutput.toString(AnsiStyle.BOLD, AnsiColor.YELLOW, msg, AnsiColor.DEFAULT));
     }
 
     public static void error(String msg) {
-        log.error(AnsiOutput.toString(AnsiColor.RED, msg, AnsiColor.DEFAULT));
+        log.error(AnsiOutput.toString(AnsiStyle.BOLD, AnsiColor.RED, msg, AnsiColor.DEFAULT));
     }
 }
